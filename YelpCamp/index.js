e6efecd3ae9d60e84c5c5a2be993b85945bb4c5a
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-12-25 10:27:05 +0700

    Add ejs-mate and all crud routes

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -2,6 +2,8 @@ const PORT = 3000;
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 
 const Campground = require("./models/campground");
 
@@ -18,10 +20,12 @@ mongoose
 
 const absPath = (relPath) => path.join(__dirname, relPath);
 
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", absPath("views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(absPath("public")));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     // TODO: add this page
@@ -37,15 +41,30 @@ app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 });
 
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.put("/campgrounds/:id", async (req,res) => {
+    await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+    res.redirect("/campgrounds/" + req.params.id);
+})
+
+app.delete("/campgrounds/:id", async  (req, res) => {
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect("/campgrounds");
+})
+
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
 });
 
-app.post("/campgrounds", async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
 });
 
 // 404 not found page
